5ca768f44e3e91c608633a609b2bebdcf5e2a5d0
Marschallne Kekesi Amata <kekiami@gmail.com>
2023-12-20 17:39:52 +0100

    oldest angular version to solve problems with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { DayService } from './services/day.service';
-import { Today } from './models/today';
-import { Tomorrow } from './models/tomorrow';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularFrontendExam';
-  actualToday:Today=new Today();
-  actualTomorrow: Tomorrow=new Tomorrow();
-
-  constructor(public service: DayService){
-
-  }
+  title = 'frontendexamangular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-
-
-import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import {environment} from 'src/environments/environment';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -18,11 +14,9 @@ import {environment} from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    AngularFireModule.initializeApp(environment.firebase),
-
+    provideFirebaseApp(() => initializeApp({"projectId":"frontendexam-c8c0f","appId":"1:992813250466:web:b084f6253a379291fd24fb","databaseURL":"https://frontendexam-c8c0f-default-rtdb.firebaseio.com","storageBucket":"frontendexam-c8c0f.appspot.com","apiKey":"AIzaSyCEn1_SdvEh7g4uLU6-Yfp5DuNDTYNOkJY","authDomain":"frontendexam-c8c0f.firebaseapp.com","messagingSenderId":"992813250466"})),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
